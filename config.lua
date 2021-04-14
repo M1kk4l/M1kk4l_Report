34f10db31ca85585ef676f2aa74551d8f2b13e08
M1kk4l <70455480+M1kk4l@users.noreply.github.com>
2021-04-15 03:11:31 +1300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Webhook = "https://discord.com/api/webhooks/830546639659073617/skceOo_7GHEYdfQ72qeHnwzSVGY3Sh51OMzyrleI9DAxr86ziwy1SOO5-XFO69-70lxr"
+
+Config.pNotify = true
+
+Config.mythic_notify = false
